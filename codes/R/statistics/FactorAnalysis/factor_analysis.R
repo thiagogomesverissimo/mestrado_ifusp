eca255af1050a8f2e7fc6adf8d81c1d39b2d6b6a
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2015-08-25 16:47:57 -0300

    Mudanças para apresentação IAG

--- codes/R/statistics/FactorAnalysis/factor_analysis.R
+++ codes/R/statistics/FactorAnalysis/factor_analysis.R
@@ -4,21 +4,9 @@ source("myfunctions/load.R")
 residencial = c('RFcH','RGcH','RIcH')   
 traffic = c('TFcH','TGcH','TIcH')
 
-# All conditions
-conditions<-c(residencial,traffic)
+FactorAnalysis('RFsH',5)
+FactorAnalysis('TFsH',5)
+FactorAnalysis('RGsH',5)
+FactorAnalysis('TGsH',5)
 
-# Inclui tabelas sem os meses do Harmathan
-conditions = c(conditions,gsub('c','s',conditions))
-
-for(i in conditions){
-  FactorAnalysis(i,5) 
-}
-
-# As exceções (que não são 6):
-# FactorAnalysis('RFcH',4)
-# FactorAnalysis('RGcH',5)
- FactorAnalysis('RGsH',4)
-# FactorAnalysis('RIsH',5)
-# FactorAnalysis('TGcH',5)
-# FactorAnalysis('TGsH',5)
- FactorAnalysis('TFsH',4)
+FactorAnalysis('TIcH',5)
