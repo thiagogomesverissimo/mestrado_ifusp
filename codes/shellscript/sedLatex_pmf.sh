254974a0a1da59f0e96a112056589a57a4c76a8b
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2015-08-28 10:56:56 -0300

    IAG: Conclusão melhoradas

--- codes/shellscript/sedLatex_pmf.sh
+++ codes/shellscript/sedLatex_pmf.sh
@@ -6,20 +6,20 @@ cd $(dirname $0)
 
 ## RFsH
 FILE='../../outputs/RFsH_contribution.tex'
-sed -i s/'Factor.1'/'americo'/g  $FILE
+sed -i s/'Factor.1'/'Veículo leve'/g  $FILE
 sed -i s/'Factor.2'/Biomassa/g  $FILE
 sed -i s/'Factor.3'/Mar/g  $FILE
 sed -i s/'Factor.4'/Solo/g  $FILE
-sed -i s/'Factor.5'/'americo'/g  $FILE
-sed -i s/RFsH/'$MP_{2.5}$ bairro'/g $FILE
+sed -i s/'Factor.5'/'Veículo pesado'/g  $FILE
+sed -i s/RFsH/'$MP_{2.5}$ baiiro'/g $FILE
 
 ## TFsH
 FILE='../../outputs/TFsH_contribution.tex'
-sed -i s/'Factor.1'/Biomassa/g  $FILE
-sed -i s/'Factor.2'/'americo'/g  $FILE
+sed -i s/'Factor.1'/'Veículo pesado'/g  $FILE
+sed -i s/'Factor.2'/Biomassa/g  $FILE
 sed -i s/'Factor.3'/Solo/g  $FILE
-sed -i s/'Factor.4'/'americo'/g  $FILE
-sed -i s/'Factor.5'/'americo'/g  $FILE
+sed -i s/'Factor.4'/Mar/g  $FILE
+sed -i s/'Factor.5'/'Veículo leve'/g  $FILE
 sed -i s/TFsH/'$MP_{2.5}$ avenida'/g $FILE
 
 ## RGsH
@@ -42,20 +42,20 @@ sed -i s/'Factor.5'/Biomassa/g  $FILE
 
 ## RFsH
 FILE='../../outputs/RFsH_profiles_percent_species.tex'
-sed -i s/'Factor.1'/'americo'/g  $FILE
+sed -i s/'Factor.1'/'Veículo leve'/g  $FILE
 sed -i s/'Factor.2'/Biomassa/g  $FILE
 sed -i s/'Factor.3'/Mar/g  $FILE
 sed -i s/'Factor.4'/Solo/g  $FILE
-sed -i s/'Factor.5'/'americo'/g  $FILE
-sed -i s/TFsH/'$MP_{2.5}$ bairro'/g $FILE
+sed -i s/'Factor.5'/'Veículo pesado'/g  $FILE
+sed -i s/RFsH/'$MP_{2.5}$ bairro'/g $FILE
 
 ## TFsH
 FILE='../../outputs/TFsH_profiles_percent_species.tex'
-sed -i s/'Factor.1'/Biomassa/g  $FILE
-sed -i s/'Factor.2'/'americo'/g  $FILE
+sed -i s/'Factor.1'/'Veículo pesado'/g  $FILE
+sed -i s/'Factor.2'/Biomassa/g  $FILE
 sed -i s/'Factor.3'/Solo/g  $FILE
-sed -i s/'Factor.4'/'americo'/g  $FILE
-sed -i s/'Factor.5'/'americo'/g  $FILE
+sed -i s/'Factor.4'/Mar/g  $FILE
+sed -i s/'Factor.5'/'Veículo leve'/g  $FILE
 sed -i s/TFsH/'$MP_{2.5}$ avenida'/g $FILE
 
 ## RGsH
