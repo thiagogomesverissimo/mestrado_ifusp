458e50e3ef3d5ec950664ed2b8093ba220deadbf
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2015-12-21 23:28:28 -0200

    código refatorado - eliminado o máximo de funções eval

--- codes/R/organizing/preparePMF_FA.R
+++ codes/R/organizing/preparePMF_FA.R
@@ -11,10 +11,13 @@ conditions<-c(residencial,traffic)
 # Inclui tabelas sem os meses do Harmathan
 conditions = c(conditions,gsub('c','s',conditions))
 
+conditions_mass = list()
+conditions_unc = list()
+
 # Lendo arquivos de concentrações
 for (i in conditions){
-  code=paste(i,"<-","read.csv('","../../outputs/concentrations/",i,".csv')",sep="")
-  code_unc=paste(i,"unc","<-","read.csv('","../../outputs/concentrations/",i,"unc.csv')",sep="")
+  code=paste("conditions_mass[['",i,"']]","<-","read.csv('","../../outputs/concentrations/",i,".csv')",sep="")
+  code_unc=paste("conditions_unc[['",i,"']]","<-","read.csv('","../../outputs/concentrations/",i,"unc.csv')",sep="")
   if(debug) print(code)
   if(debug) print(code_unc)
   eval(parse(text=code))
@@ -22,58 +25,53 @@ for (i in conditions){
 }
 
 # Eliminando colunas desnecessárias
-removidos = 'c("SampleID","SiteName","SampleType","diamesano","volumem3","Duplicate","Cr","Rb","Sr","Zr","Cu","Ni")'
-for(i in conditions){
- code=paste(i,"<-",i,"[,!names(",i,") %in% ", removidos, "]",sep="")
- code_unc=paste(i,"unc","<-",i,"unc","[,!names(",i,"unc",") %in% ", removidos ,"]",sep="")
- if(debug) print(code)
- if(debug) print(code_unc)
- eval(parse(text=code))
- eval(parse(text=code_unc))
+removidos = c("SampleID","SiteName","SampleType","diamesano","volumem3",
+              "Duplicate","Cr","Rb","Sr","Zr","Cu","Ni")
+for(i in names(conditions_mass)){
+  
+  #i = 'RFcH'
+  data = conditions_mass[[i]]
+  data_unc = conditions_unc[[i]]
+  
+  # Remove colunas desnecessárias
+  data = data[,!names(data) %in% removidos]
+  data_unc = data_unc[,!names(data_unc) %in% removidos]
+  
+  # Transforma a coluna Date em tipo date para ordenação
+  data$Date = as.POSIXct(strptime(data$Date,format='%Y-%m-%d %H:%M:%S',tz='GMT'))
+  data_unc$Date = as.POSIXct(strptime(data_unc$Date,format='%Y-%m-%d %H:%M:%S',tz='GMT'))
+  
+  # ordena por Date, pois o PMF só aceita se estiver ordenado
+  data<-data[order(data$Date),]
+  data_unc<-data_unc[order(data_unc$Date),]
+
+  # data no formato do PMF como rownames
+  rownames(data) <- strftime(data$Date,format='%d/%m/%Y %H:%M')
+  rownames(data_unc) <- strftime(data_unc$Date,format='%d/%m/%Y %H:%M')
+  
+  # remove coluna Date
+  data = data[,-1]
+  data_unc = data_unc[,-1]
+  
+  # muda na lista de fato
+  conditions_mass[[i]] = data
+  conditions_unc[[i]] = data_unc
 }
 
-# Transformando as colunas Date para tipo R-Date. 
-for(i in conditions){
-  #Formatando a coluna dates com formato de data
-  code=paste(i,"$Date","<-","as.POSIXct(strptime(",i,"$Date",",format='%Y-%m-%d %H:%M:%S',tz='GMT'))",sep="")
-  code_unc=paste(i,"unc","$Date","<-","as.POSIXct(strptime(",i,"unc","$Date",",format='%Y-%m-%d %H:%M:%S',tz='GMT'))",sep="")
-  if(debug) print(code)
-  if(debug) print(code_unc)
-  eval(parse(text=code))
-  eval(parse(text=code_unc))
-}
-
-# Ordenando pela coluna Date, pois o PMF só aceita se estive ordenado.
-for(i in conditions){
-  #Formatando a coluna dates com formato de data
-  code=paste(i,"<-",i,"[order(",i,"$Date),]",sep="")
-  code_unc=paste(i,"unc<-",i,"unc[order(",i,"unc$Date),]",sep="")
-  if(debug) print(code)
-  if(debug) print(code_unc)
-  eval(parse(text=code))
-  eval(parse(text=code_unc))
-}
-
-# Mudando coluna Dates para character e no formato do PMF: %d/%m/%Y %H:%M
-for(i in conditions){
-  code=paste(i,"$Date","<-","strftime(",i,"unc","$Date",",format='%d/%m/%Y %H:%M')",sep="")
-  code_unc=paste(i,"unc","$Date","<-","strftime(",i,"unc","$Date",",format='%d/%m/%Y %H:%M')",sep="")
-  if(debug) print(code)
-  if(debug) print(code_unc)
-  eval(parse(text=code))
-  eval(parse(text=code_unc))
+# Acrescenta 8% da concentração no erro 
+conditions_unc8 = list()
+for(i in names(conditions_mass)) {
+  conditions_unc8[[i]] = sqrt( (conditions_mass[[i]]*0.08)^2 + conditions_unc[[i]]^2 )
 }
 
 # Cria diretório PMF
 if(!('pmf_fa' %in% list.files("../../outputs/"))) dir.create("../../outputs/pmf_fa")
 
 # Salvar os arquivos para análise PMF/FA, não pode ter aspas
-for (i in conditions){
-  code=paste("write.csv(",i,",'../../outputs/pmf_fa/",i,".csv',","row.names=FALSE,quote=F)",sep="")
-  code_unc=paste("write.csv(",i,"unc",",'../../outputs/pmf_fa/",i,"unc.csv',","row.names=FALSE,quote=F)",sep="")
-  if(debug) print(code)
-  if(debug) print(code_unc)
-  eval(parse(text=code))
-  eval(parse(text=code_unc))
+for(i in names(conditions_mass)){
+  folder = '../../outputs/pmf_fa/'
+  write.csv(conditions_mass[[i]], paste(folder,i,'.csv',sep=''),quote=F)
+  write.csv(conditions_unc[[i]], paste(folder,i,'unc.csv',sep=''),quote=F)
+  write.csv(conditions_unc8[[i]], paste(folder,i,'unc8.csv',sep=''),quote=F)
 }
 
