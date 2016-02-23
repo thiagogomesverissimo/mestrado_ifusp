05e50bb00fcf753165760e72ace6250bad89dd76
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2016-02-23 19:36:48 -0300

    coeficientes corrigidos

--- codes/R/graphs/edxCalibration.R
+++ codes/R/graphs/edxCalibration.R
@@ -14,7 +14,7 @@ legenda = c('ajuste1: Z 11-26',
 edxCalibration('K2010MaiAkerr','K',file_medidos,cores,legenda,coefs1,coefs2)
 
 # Linha L
-coefs1 = c(-0.90276721,0.10112016,-0.0043561,8.89436792E-05,-8.42128969E-07,0.000000003)
+coefs1 = c(-0.90276721,0.10112016,-0.0043561,0.0000889436792,-0.000000842128969,0.000000003)
 file_medidos = '../../inputs/edxCalibration/americo/L2010MaioMedidosAkerr.csv'
 cores = col=c('blue','black')
 legenda = c('ajuste1: Z 11-26','medidos')
@@ -33,7 +33,7 @@ legenda = c('ajuste1: Z 11-26',
 edxCalibration('K2010NovAkerr','K',file_medidos,cores,legenda,coefs1,coefs2)
 
 # Linha L
-coefs1 = c(-0.852155,0.095432,-0.0041149,8.41603E-05,-7.9806E-07,2.8404E-09)
+coefs1 = c(-0.852155,0.095432,-0.0041149,0.0000841603,-0.00000079806,0.0000000028404)
 file_medidos = '../../inputs/edxCalibration/americo/L2010NovMedidosAkerr.csv'
 cores = col=c('blue','black')
 legenda = c('ajuste1: Z 11-26','medidos')
@@ -42,8 +42,8 @@ edxCalibration('2010NovAkerr','L',file_medidos,cores,legenda,coefs1)
 #### Calibração Abril de 2011 ####
 
 # Linha K
-coefs1 = c(0.15987616,-0.026726302,9.55E-04,1.45E-05)
-coefs2 = c(-4.3221494, 0.4083262,-0.011499903, 1.04E-04)
+coefs1 = c(0.15987616,-0.026726302,0.00095542146,0.000014469997)
+coefs2 = c(-4.3221494, 0.4083262,-0.011499903, 0.00010389019)
 file_medidos = '../../inputs/edxCalibration/americo/K2011AbrMedidosAkerr.csv'
 cores = col=c('blue','red','black')
 legenda = c('ajuste1: Z 11-26',
@@ -52,7 +52,7 @@ legenda = c('ajuste1: Z 11-26',
 edxCalibration('K2011AbrAkerr','K',file_medidos,cores,legenda,coefs1,coefs2)
 
 # Linha L
-coefs1 = c(-0.71619926,0.07987028,-0.003424948,6.95E-05,-6.49E-07,2.27E-09)
+coefs1 = c(-0.71619926,0.07987028,-0.003424948,0.00006946821,-0.00000064936,0.0000000022665)
 file_medidos = '../../inputs/edxCalibration/americo/L2011AbrMedidosAkerr.csv'
 cores = col=c('blue','black')
 legenda = c('ajuste1: Z 11-26','medidos')
---
+++ codes/R/organizing/samplesBetweenCalibration.R
@@ -0,0 +1,36 @@
+rm(list=ls())
+source("myfunctions/load.R")
+
+# get csv files names
+csvfolder = '/home/thiago/remota/arquivados/pesquisa/mestrado_ifusp/spectrum_analysis/winqxas/brancos/'
+csvfiles = c()
+for (i in list.files(csvfolder)){
+  if(grepl('csv',i)) csvfiles=append(csvfiles,i)
+}
+
+intervalo = c(as.Date('2010-05-20'),as.Date('2010-11-16'))
+#intervalo2 = c(as.Date('2010-11-16'),as.Date('2011-04-08'))
+segundos = 0
+
+for (csvfile in csvfiles) {
+  print(csvfile)
+  # Leitura do arquivo
+  linhas = read.csv(paste(csvfolder,csvfile,sep=''),header = F)
+  
+  # get date
+  dateline = unlist(strsplit(as.character(linhas[5,1]), split=" "))
+  #print(as.Date(dateline[9]))
+  
+  # get time
+  timeline = unlist(strsplit(as.character(linhas[13,1]), split=" "))
+  #print(timeline[40])
+  
+  if ( (as.Date(dateline[9]) >= intervalo[1]) & (as.Date(dateline[9]) <= intervalo[2])) {
+    segundos = as.numeric(timeline[40]) + segundos
+  }
+}
+
+
+
+
+
