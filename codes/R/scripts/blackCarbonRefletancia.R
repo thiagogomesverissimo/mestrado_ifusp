7fb80aa55f00577a3543465dac359d8857e98239
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2016-04-19 20:55:07 -0300

    u

--- codes/R/scripts/blackCarbonRefletancia.R
+++ codes/R/scripts/blackCarbonRefletancia.R
@@ -1,16 +1,18 @@
 #rm(list=ls())
 source("myfunctions/load.R")
 
+######################### Filtros do IFUSP
 ref2007 = read.csv('../../inputs/BlackCarbon/calibration/Refletancia2007.csv')
 x = ref2007$Log10Ref
 y = ref2007$ug_cm2
 y_erro = ref2007$erro_ug_cm2 
 
 # Ajuste polinomial, vou ajustar, mas vou mostrar a do Lapat
-model <- lm(y ~ poly(x,2,raw=TRUE))
+#model <- lm(y ~ poly(x,2,raw=TRUE))
+model <- lm(y ~ poly(x,1,raw=TRUE))
 coefs_thiago = model$coefficients
 coefs_thiago = as.vector(coefs_thiago)
-coefs_lapat = c(82.8046,72.8235,15.7169)
+#coefs_lapat = c(82.8046,72.8235,15.7169)
 
 pdf('../../outputs/CalibracaoRefletancia2007Lapat.pdf')
 # Gráfico 'vazio'
@@ -34,13 +36,15 @@ lines(p,xlim = c(1.5,2),col='red')
 errbar(x,y,y + y_erro, y - y_erro, pch=20, add=TRUE)
 
 # legenda
-legend("topleft", legend = expression(a + bx + cx^2), cex=0.8, bty = "n")
+#legend("topleft", legend = expression(a + bx + cx^2), cex=0.8, bty = "n")
+legend("topleft", legend = expression(a + bx), cex=0.8, bty = "n")
 
-legenda = paste(letters[1:3],format(coefs_lapat, scientific=T),sep=' = ')
+#legenda = paste(letters[1:3],format(coefs_lapat, scientific=T),sep=' = ')
+legenda = paste(letters[1:2],format(coefs_thiago, scientific=T),sep=' = ')
 legenda = paste(legenda,'\n',sep='')
 legenda = paste(legenda,collapse=" ")
 legenda = gsub('\\.',',',legenda)
-legenda = paste('\n Coeficientes do ajuste \n polinomial grau 2: \n\n',legenda)
+legenda = paste('\n Coeficientes do ajuste linear: \n\n',legenda)
 #legenda = c(legenda,'Medidas')
 
 #legend("bottomright", legend = legenda, inset=c(-0.2,0), col=c('red','black'), pch = 15, cex=0.7, bty = "n")
@@ -58,3 +62,52 @@ print(xtable(tabela),
       include.rownames = F, 
       sanitize.text.function = identity,
       file="../../outputs/CalibracaoRefletancia2007.tex")
+
+############################ Filtros Cetesb 2012 - Testes
+cetesb2012= read.csv('../../inputs/BlackCarbon/calibration/cetesb2012.csv')
+x = log10(cetesb2012$refletencia)
+y = cetesb2012$massa_ugcm2
+y_erro = cetesb2012$erro_ugcm2
+
+# Ajuste polinomial, vou ajustar, mas vou mostrar a do Lapat
+#model <- lm(y ~ poly(x,2,raw=TRUE))
+model <- lm(y ~ poly(x,1,raw=TRUE))
+coefs_thiago = model$coefficients
+coefs_thiago = as.vector(coefs_thiago)
+
+pdf('../../outputs/CalibracaoCetesb2012.pdf')
+# Gráfico 'vazio'
+mar.default <- c(5,4,4,2) + 0.5
+par(mar = mar.default + c(0, 3, 0, 0))
+plot(0,0,
+     xlim = c(0.4,2),
+     ylim = c(0,100),
+     type = "n",
+     xlab = 'Log10 da refletância em %',
+     ylab = expression(frac(mu*g,cm^2)),
+     axes=F)
+
+axis(side=1, at=seq(0.4, 2, by=0.05))
+axis(side=2, at=seq(0, 100, by=10))
+
+p <- polynomial(coefs_thiago)
+lines(p,xlim = c(0.4,2),col='red')
+
+errbar(x,y,y + y_erro, y - y_erro, pch=20, add=TRUE)
+
+# legenda
+#legend("topleft", legend = expression(a + bx + cx^2), cex=0.8, bty = "n")
+legend("topleft", legend = expression(a + bx), cex=0.8, bty = "n")
+
+#legenda = paste(letters[1:3],format(coefs_lapat, scientific=T),sep=' = ')
+legenda = paste(letters[1:2],format(coefs_thiago, scientific=T),sep=' = ')
+legenda = paste(legenda,'\n',sep='')
+legenda = paste(legenda,collapse=" ")
+legenda = gsub('\\.',',',legenda)
+legenda = paste('\n Coeficientes do ajuste linear: \n\n',legenda)
+#legenda = c(legenda,'Medidas')
+
+#legend("bottomright", legend = legenda, inset=c(-0.2,0), col=c('red','black'), pch = 15, cex=0.7, bty = "n")
+legend("topright", legend = legenda, col='red',inset=c(0,-0.1),pch = 15, cex=0.8, bty = "n")
+
+dev.off()
