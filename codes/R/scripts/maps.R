994f216c775c9fea25a6603da8ccfe29fb8278d7
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2016-05-09 23:35:06 -0300

    apêndice corrigido

--- codes/R/scripts/maps.R
+++ codes/R/scripts/maps.R
@@ -1,29 +1,29 @@
-#rm(list=ls())
+rm(list=ls())
 source("myfunctions/load.R")
 
 pontos = read.csv('../../inputs/geocode_nima.csv')
 pontos = pontos[,1:3]
+amostragem = pontos[nrow(pontos),]
 
 # site para pegar box http://nominatim.openstreetmap.org/
 # onde o viewbox: left/bottom e right/top em lat lon
 # mouse position: lat lon
 
 # na biblioteca ggmap: left/bottom e right/top em lon,lat
-location = c(-0.40649,5.51074,0.07759,5.74991)
-#location = c(-0.31345,5.51347,-0.07141,5.63307)
-#location = c(-0.47035,5.42598,0.01373,5.66518)
-#location = c(-0.82672,5.27968,0.14145,5.75811)
-#location = c(-1.57104,4.99245,0.36530,5.94936)
+location_osm = c(-0.40649,5.51074,0.07759,5.74991)
+location_google = c(-0.28,5.5,0.09,5.7)
 
 # Fetch the map
-accra = get_map(location = location, source = "osm",filename = '/tmp/accra')
+#accra = get_map(location = location, source = "osm",filename = '/tmp/accra',zoom = 10)
+accra = get_map(location = location_google, source = "google",filename = '/tmp/accra')
 
 pdf('../../outputs/accra_sources.pdf')
 # Draw the map
 accraMap = ggmap(accra)
 
 # Add the points layer
-accraMap = accraMap + geom_point(data = pontos, aes(x = longitude, y = latitude), size = 2)
+accraMap = accraMap + geom_point(data = pontos, aes(x = longitude, y = latitude), size = 4, color = "black")
+accraMap = accraMap + geom_point(data = amostragem, aes(x = longitude, y = latitude), size = 4, color = "red")
 
 # Add the labels
 accraMap + geom_text(data = pontos, aes(label = name, x = longitude+.001, y = latitude), hjust = -0.15)
